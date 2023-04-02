1b604bcbe593fba504cfe18471cfa6f36ed08ca4
Heruwim <heruwim25@gmail.com>
2023-04-02 14:30:14 +0300

    Script

--- Assets/PlayerShooter.cs
+++ Assets/PlayerShooter.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
-[RequireComponent(typeof(PlayerInput))]
 
 public class PlayerShooter : MonoBehaviour
 {
-    public void OnShoot(InputAction.CallbackContext context)
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+
+        _playerInput.Player.Shoot.performed += ctx => OnShoot();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    public void OnShoot()
     {
         Debug.Log("Shoot");
     }
